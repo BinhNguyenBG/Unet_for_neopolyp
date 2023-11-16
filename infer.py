05bf5010fa315ce564c2fd3d7da8c7024c8517da
BinhNguyenBG <130740759+BinhNguyenBG@users.noreply.github.com>
2023-11-16 20:56:04 +0700

    Add files via upload

---
+++ infer.py
@@ -0,0 +1,73 @@
+import os 
+import sys
+
+from torchsummary import summary
+from torchgeometry.losses import one_hot
+import pandas as pd
+import numpy as np
+from PIL import Image
+import cv2
+import time
+import imageio
+import matplotlib.pyplot as plt
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch import Tensor
+from torch.utils.data import Dataset, DataLoader, random_split, ConcatDataset
+from torchvision.transforms import *
+from collections import OrderedDict
+from backbones_unet.model.unet import Unet
+from mask_io import mask2string
+from TestData import UNetTestDataClass
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load the model
+model = Unet(
+    in_channels=3,           
+    num_classes=3,            
+)
+
+model.to(device)
+
+# Load the pretrained model 
+check_point = torch.load('model.pth', map_location=device)
+model.load_state_dict(check_point['model'])
+
+
+# Transform for the test set
+transform = Compose([Resize((512, 512), interpolation=InterpolationMode.BILINEAR),
+                     PILToTensor()])
+
+path = '/kaggle/input/bkai-igh-neopolyp/test/test/'
+unet_test_dataset = UNetTestDataClass(path, transform)
+test_dataloader = DataLoader(unet_test_dataset, batch_size=8, shuffle=True)
+
+model.eval()
+if not os.path.isdir("/kaggle/working/predicted_masks"):
+    os.mkdir("/kaggle/working/predicted_masks")
+for _, (img, path, H, W) in enumerate(test_dataloader):
+    a = path
+    b = img
+    h = H
+    w = W
+    
+    with torch.no_grad():
+        predicted_mask = model(b)
+    for i in range(len(a)):
+        image_id = a[i].split('/')[-1].split('.')[0]
+        filename = image_id + ".png"
+        mask2img = Resize((h[i].item(), w[i].item()), interpolation=InterpolationMode.NEAREST)(ToPILImage()(F.one_hot(torch.argmax(predicted_mask[i], 0)).permute(2, 0, 1).float()))
+        mask2img.save(os.path.join("/kaggle/working/predicted_masks/", filename))
+
+MASK_DIR_PATH = '/kaggle/working/predicted_masks' # change this to the path to your output mask folder
+dir = MASK_DIR_PATH
+res = mask2string(dir)
+df = pd.DataFrame(columns=['Id', 'Expected'])
+df['Id'] = res['ids']
+df['Expected'] = res['strings']
+df.to_csv(r'output.csv', index=False)
