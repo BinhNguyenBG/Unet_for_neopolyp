c7bfdcddee66dda83596e69be7570d89463c931e
BinhNguyenBG <130740759+BinhNguyenBG@users.noreply.github.com>
2023-11-16 22:43:13 +0700

    Update mask_io.py

--- mask_io.py
+++ mask_io.py
@@ -7,8 +7,7 @@ def rle_to_string(runs):
 
 def rle_encode_one_mask(mask):
     pixels = mask.flatten()
-    pixels[pixels > 225] = 255
-    pixels[pixels <= 225] = 0
+    pixels[pixels > 0] = 255
     use_padding = False
     if pixels[0] or pixels[-1]:
         use_padding = True
@@ -22,33 +21,25 @@ def rle_encode_one_mask(mask):
     rle[1::2] = rle[1::2] - rle[:-1:2]
     return rle_to_string(rle)
 
-def rle2mask(mask_rle, shape=(3,3)):
-    s = mask_rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
-    ends = starts + lengths
-    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 1
-    return img.reshape(shape).T
-
 def mask2string(dir):
-    result_str = []
-    idx = []
+    ## mask --> string
+    strings = []
+    ids = []
     ws, hs = [[] for i in range(2)]
     for image_id in os.listdir(dir):
         id = image_id.split('.')[0]
         path = os.path.join(dir, image_id)
+        print(path)
         img = cv2.imread(path)[:,:,::-1]
         h, w = img.shape[0], img.shape[1]
         for channel in range(2):
             ws.append(w)
             hs.append(h)
-            idx.append(f'{id}_{channel}')
+            ids.append(f'{id}_{channel}')
             string = rle_encode_one_mask(img[:,:,channel])
-            result_str.append(string)
+            strings.append(string)
     r = {
-        'idx': idx,
-        'result_str': result_str,
+        'ids': ids,
+        'strings': strings,
     }
     return r
